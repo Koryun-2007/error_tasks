f0a2a3453f1215280283964555aa60a49ee77212
Koryun-2007 <koryunavetisyan3.@gmail.com>
2024-09-15 19:03:47 +0400

    add codeChanging and isPositive and reverseString

---
+++ reverseString.js
@@ -0,0 +1,14 @@
+function reverseString (s){
+    try{
+        s = s.split('').reverse().join('') 
+    }catch (error){
+        console.log(`Error ${Error.message}`);
+        
+    }finally{
+        console.log(s);
+        
+    }
+}
+reverseString("hello");
+reverseString(1234);
+
